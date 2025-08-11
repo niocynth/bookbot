5abcce2353785ad72676acd5945f5dda507a5226
Niocynth <dev@niocynth.io>
2025-08-11 19:18:23 +0100

    Added command line arguments

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_word_count
 from stats import get_character_dict
 from stats import convert_character_count
@@ -11,12 +12,15 @@ def get_book_text(filepath):
     return file_contents
 
 def main():
-    book_loc = "books/frankenstein.txt"
+    # catch invalid args
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_loc = sys.argv.pop()
     book = get_book_text(book_loc)
     word_count = get_word_count(book)
     character_dict = get_character_dict(book)
     char_list = convert_character_count(character_dict)
- #   char_key = sort_on(char_list)
     character_list = sort_char_list(char_list)
     gen_report(book_loc, word_count, character_list)
 
