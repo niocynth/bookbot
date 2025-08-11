3c0bef109e8185e0cefd449a6a856823c836c7bd
niocynth <dev@niocynth.io>
2025-08-11 08:56:00 +0100

    Added report.py to clean up main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from stats import get_word_count
 from stats import get_character_count
-from stats import sort_character_count
+from report import gen_report
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -11,13 +11,7 @@ def main():
     book_loc = "books/frankenstein.txt"
     book = get_book_text(book_loc)
     word_count = get_word_count(book)
-    character_count = get_character_count(book)
-   # sorted_character_count = sort_character_count(character_count)
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {book_loc}...")
-    print('----------- Word Count ----------')
-    print(f"Found {word_count} total words")
-    print("--------- Character Count -------")
-    print(character_count)
+    character_list = get_character_count(book)
+    gen_report(book_loc, word_count, character_list)
 
 main()
---
+++ report.py
@@ -0,0 +1,7 @@
+def gen_report(book_loc, word_count, character_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_loc}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    print(character_list)
